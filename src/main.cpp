b2e046aba938447dd1d94c1f5798e1404f717149
m4tex <91064036+m4tex@users.noreply.github.com>
2023-01-23 22:41:17 +0000

    Going for OpenGL for the cross compability :> (old windows.h code in a separate branch)

--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,37 @@
-#include <windows.h>
-#include <string>
-#include <chrono>
-#include "../include/engine-methods.h"
-#include "../include/debug-console.h"
-
 #define REND_WIDTH 512
 #define REND_HEIGHT 512
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR args, int nCmdShow) {
-    //#region Creating and showing the engine window
-    const std::string WINDOW_NAME = "Solar Engine";
-    const char CLASS_NAME[] = "SolarEngineWindow";
+int main()
+{
+    GLFWwindow* window;
 
-    WNDCLASS wc = {};
-    wc.hbrBackground = GetSysColorBrush(BLACK_BRUSH);
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInst;
-    wc.lpszClassName = CLASS_NAME;
+    if (!glfwInit())
+        return -1;
 
-    RegisterClass(&wc);
-
-    RECT rect = { 0, 0, REND_WIDTH, REND_HEIGHT };
-    AdjustWindowRect(&rect, WS_CAPTION, false);
-
-    HWND hwnd = CreateWindow(   CLASS_NAME,
-                                WINDOW_NAME.c_str(),
-                                WS_SYSMENU | WS_CAPTION,
-                                CW_USEDEFAULT, CW_USEDEFAULT,
-                                rect.right - rect.left, rect.bottom - rect.top,
-                                nullptr,
-                                nullptr,
-                                hInst,
-                                nullptr);
-
-    ShowWindow(hwnd, SW_SHOWDEFAULT);
-    SetFocus(hwnd);
-    //#endregion
-
-    //#region Console init and loop fields
-    Console::Attach(hwnd);
-    Console::Log("-------------------------\n"
-                 "  Solar Engine by m4tex \n"
-                 "-------------------------\n"
-                 "C - toggle console\n"
-                 "T - terminate/quit\n"
-                 "I - input\n"
-                 "-------------------------");
-
-    MSG msg;
-    PAINTSTRUCT ps;
-    int FPS;
-    auto t_fps_old = std::chrono::high_resolution_clock::now();
-    auto *frameBuffer = new COLORREF[REND_WIDTH*REND_HEIGHT] { 0 };
-    //#endregion
-
-    //Engine loop
-    while (true) {
-        //#region Message Handling
-        if (PeekMessageA(&msg, nullptr, 0, 0, PM_REMOVE)) {
-            TranslateMessage(&msg);
-            DispatchMessageA(&msg);
-        }
-
-        if (msg.message == WM_QUIT)
-            break;
-        //#endregion
-
-        //Frame painting
-        EngineMethods::DrawLine(
-                                {0, 0},
-                                {512, 100},
-                                RGB(255, 255, 0),
-                                frameBuffer,
-                                REND_WIDTH);
-
-        //#region Draw a bitmap from frameBuffer
-        HBITMAP bitmap = CreateBitmap(REND_WIDTH,
-                                      REND_HEIGHT,
-                                      1,
-                                      8*4,
-                                      frameBuffer);
-
-        HDC deviceCtx = BeginPaint(hwnd, &ps);
-        HDC srcHdc = CreateCompatibleDC(deviceCtx);
-
-        SelectObject(srcHdc, bitmap);
-
-        BitBlt(deviceCtx,
-               0, 0,
-               REND_WIDTH, REND_HEIGHT,
-               srcHdc,
-               0, 0,
-               SRCCOPY);
-
-        EndPaint(hwnd, &ps);
-
-        DeleteObject(bitmap);
-        DeleteDC(srcHdc);
-        //#endregion
-
-        //#region FPS Display
-        auto t_fps_new = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> dt = t_fps_new - t_fps_old;
-        FPS = 1/dt.count();
-        SetWindowTextA(hwnd, (WINDOW_NAME + " FPS: " + std::to_string(FPS)).c_str());
-        t_fps_old = t_fps_new;
-        //#endregion
+    window = glfwCreateWindow(640, 480, "Poggies", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
     }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initiate Glew" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        //hook and listen for it globally? would be nice for debugging.... I wrote this, but now I have no idea what I meant xd
-        case WM_CHAR:
-            switch(EngineMethods::ToLower((char)wParam)) {
-                case 'c':
-                    Console::ToggleVis();
-                    break;
-
-                case 't':
-                    PostQuitMessage(0);
-                    break;
-            }
-            return 0;
-
-
-        case WM_PAINT:
-//            Console::Log("Main Proc");
-            return 0;
-
-
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
