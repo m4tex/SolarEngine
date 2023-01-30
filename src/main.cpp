916704e555ce3f48ec1bf648681602e1290843f6
m4tex <91064036+m4tex@users.noreply.github.com>
2023-01-30 20:54:36 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -116,6 +116,11 @@ int main()
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
     GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Poggies", NULL, NULL);
     if (!window)
     {
